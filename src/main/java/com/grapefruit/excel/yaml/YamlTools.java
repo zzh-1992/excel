a395f358e13fdc7540a7a2ec377d9c6df4c89c5f
zzh-1992 <ezzh1992@icloud.com>
2021-04-30 06:32:27 +0800

    yaml工具整理

--- src/main/java/com/grapefruit/excel/yaml/YamlTools.java
+++ src/main/java/com/grapefruit/excel/yaml/YamlTools.java
@@ -8,7 +8,10 @@ import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Grapefruit
@@ -40,38 +43,60 @@ public class YamlTools implements ApplicationContextAware {
         Yaml yaml = new Yaml(options);
 
         // 原文件
-        String sourcePath = "C:\\Users\\zhangzhihuang\\Desktop\\application.yml";
+        String sourcePath = "/Users/zhihuangzhang/IdeaProjects/excel/src/main/resources/application.yaml";
 
         // 输出路径
-        String path = "C:\\Users\\zhangzhihuang\\Desktop\\a.yml";
+        String path = "/Users/zhihuangzhang/IdeaProjects/excel/src/main/java/com/grapefruit/excel/yaml/output.yaml";
 
         // 输出
-        yaml.dump(getSource(sourcePath),new FileWriter(path));
+        yaml.dump(getSourceFromYaml(sourcePath),new FileWriter(path));
     }
 
-    public static HashMap getFile(){
-        File baseFile = new File("/Users/zhihuangzhang/IdeaProjects/excel/src/main/java/com/grapefruit/excel/file");
-        File[] files = baseFile.listFiles();
-        HashMap keyValue = new HashMap<>();
-        for (File file:files) {
-            if(file.getName().endsWith(".yaml")){
-                keyValue.putAll(getSource(file.getAbsolutePath()));
-            }
-        }
+    /**
+     * 在给定文件夹里获取所有的yaml文件并转换为map
+     *
+     * @return map
+     */
+    public static HashMap<String,String> getFile(){
+        String baseDirectory = "/Users/zhihuangzhang/IdeaProjects/excel/src/main/java/com/grapefruit/excel/file";
+
+        List<File> yamlFile = getYamlFile(baseDirectory);
+
+        HashMap<String,String> keyValue = new HashMap<>();
+        yamlFile.stream()
+                .filter(file -> file.getName().endsWith(".yaml"))
+                .forEach(
+                        file -> keyValue.putAll(getSourceFromYaml(file.getAbsolutePath()))
+                );
         System.out.println(keyValue.size());
         return keyValue;
     }
 
-    public static HashMap  getSource(String fileName) {
+    /**
+     * 获取给定文件夹下的所有文件
+     *
+     * @param directoryPath 文件夹
+     * @return List<File>
+     */
+    public static List<File> getYamlFile(String directoryPath){
+        File baseFile = new File(directoryPath);
+        return Arrays.asList(Objects.requireNonNull(baseFile.listFiles()));
+    }
+
+    /**
+     * 从yaml文件里获取值并返回map
+     *
+     * @param fileName yaml文件的绝对路径
+     * @return Map
+     */
+    private static HashMap<String,String> getSourceFromYaml(String fileName) {
         Yaml yaml = new Yaml();
         FileInputStream fis = null;
         try {
-            //fis = new FileInputStream("/Users/zhihuangzhang/IdeaProjects/excel/src/main/java/com/grapefruit/excel/file/properties.yaml");
             fis = new FileInputStream(fileName);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
         return yaml.loadAs(fis, HashMap.class);
     }
 }
